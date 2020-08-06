8349e7d940272df0aa765d9c339b4624b0debcdd
Bae Jin Woo <jw969696@naver.com>
2020-08-06 12:11:06 +0900

    sort

--- 2750.py
+++ 2750.py
@@ -1,6 +1,13 @@
 num = int(input(""))
 numList = []
-for i in num:
+for i in range (0,num):
     a = int(input())
-    numList.append(i)
-print(numList)
+    numList.append(a)
+for i in range(len(numList)):
+    for j in range(i,len(numList)):
+        if(numList[i]>numList[j]):
+            tmp = numList[j]
+            numList[j] = numList[i]
+            numList[i] = tmp
+for num in numList:
+    print(num)
