466f44e1a34a9bc0e9f7f6caa33b0369ad26d232
Bae Jin Woo <jw969696@naver.com>
2020-08-18 01:25:01 +0900

    nm

--- 15649.py
+++ 15649.py
@@ -1,5 +1,23 @@
-from itertools import permutations
-N, M = map(int, input().split())
-P = permutations(range(1, N+1), M)  # iter(tuple)
-for i in P:
-    print(' '.join(map(str, i)))  # tuple -> str
+n, m = map(int, input().split(" "))
+
+
+def process(tmp_store, tmp_value):
+    if len(tmp_value) == m:
+        result = ''
+        for c in tmp_value:
+            result += (c + ' ')
+        print(result)
+        return
+
+    for i in range(0, n):
+        if tmp_store[i] == 0:
+            tmp_store[i] = 1
+            process(tmp_store, tmp_value + str(i+1))
+            tmp_store[i] = 0
+
+
+for i in range(0, n):
+    store = [0 for i in range(n)]
+    store[i] = 1
+    value = str(i+1)
+    process(store, value)
